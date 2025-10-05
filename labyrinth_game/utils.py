6d67d6de15cfeea1e04da9b56580c48694e6f756
Tuinf1 <rassokhinvladislav@mail.ru>
2025-10-05 22:53:10 +0700

    2-2.4_commit(9)

--- labyrinth_game/utils.py
+++ labyrinth_game/utils.py
@@ -82,3 +82,14 @@ def attempt_open_treasure(game_state):
         game_state['game_over'] = True
     else:
         print("Неверный код. Сундук остаётся закрытым.")
+
+def show_help():
+    print("\nДоступные команды:")
+    print("  go <direction>  - перейти в направлении (north/south/east/west)")
+    print("  look            - осмотреть текущую комнату")
+    print("  take <item>     - поднять предмет")
+    print("  use <item>      - использовать предмет из инвентаря")
+    print("  inventory       - показать инвентарь")
+    print("  solve           - попытаться решить загадку в комнате")
+    print("  quit            - выйти из игры")
+    print("  help            - показать это сообщение") 
